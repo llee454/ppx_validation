08a5456214a047598c376ea4ba83972e4a0b0171
Larry Lee <llee454@gmail.com>
2022-07-27 22:08:36 -0400

    Removed debugging output from test/main.ml

--- test/main.ml
+++ test/main.ml
@@ -10,63 +10,7 @@ type t = {
         }]
   b: int; [@validation { sort = Int_ranged; spec = { low = Some 0; high = None } }]
 }
-[@@deriving fields] [@@deriving_inline validation]
-
-let _ = (fun (_ : t) -> ())
-
-module Validations = struct
-  type stats = {
-    a_stats: Ppx_validation_runtime.Str_enum.stats;
-    b_stats: Ppx_validation_runtime.Int_ranged.stats;
-  }
-  [@@deriving fields]
-
-  let stats_init () =
-    {
-      a_stats = Ppx_validation_runtime.Str_enum.stats_init ();
-      b_stats = Ppx_validation_runtime.Int_ranged.stats_init ();
-    }
-
-  let _ = stats_init
-
-  let get_stats xs =
-    let open Ppx_validation_runtime.Report in
-    List.fold xs ~init:(stats_init ()) ~f:(fun acc x ->
-        Fields_of_stats.map
-          ~a_stats:
-            (app acc (fun s ->
-                 a x
-                 |> Fn.id
-                 |> Option.value_map ~default:s ~f:(fun x ->
-                        Ppx_validation_runtime.Str_enum.stats_update
-                          { valid_values = String.Set.of_list [ "true"; "false" ] }
-                          x s)))
-          ~b_stats:
-            (app acc (fun s ->
-                 b x
-                 |> Option.some
-                 |> Option.value_map ~default:s ~f:(fun x ->
-                        Ppx_validation_runtime.Int_ranged.stats_update { low = Some 0; high = None } x s))))
-
-  let _ = get_stats
-
-  let report_aux stats =
-    let open Ppx_validation_runtime in
-    Fields_of_stats.to_list
-      ~a_stats:
-        (Report.rep stats (fun s ->
-             [ Str_enum (Str_enum.report { valid_values = String.Set.of_list [ "true"; "false" ] } s) ]))
-      ~b_stats:
-        (Report.rep stats (fun s -> [ Int_ranged (Int_ranged.report { low = Some 0; high = None } s) ]))
-
-  let _ = report_aux
-
-  let report xs = get_stats xs |> report_aux
-
-  let _ = report
-end
-
-[@@@end]
+[@@deriving fields, validation]
 
 let xs = [ { a = Some "true"; b = 1 }; { a = Some "false"; b = 0 } ]
 
